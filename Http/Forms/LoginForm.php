41e6bd1e6bbbe1d81e914ef35d03a90bc250f954
victor braz <victor.90braz@gmail.com>
2023-10-09 16:14:18 +0200

    remove total pwd

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -15,7 +15,7 @@ class LoginForm
             $this->errors['email'] = 'Please provide a valid email address.';
         }
 
-        if (!Validator::string($attributes['password'],1,5)) {
+        if (!Validator::string($attributes['password'])) {
             $this->errors['password'] = 'Please provide a valid password.';
         }
     }
