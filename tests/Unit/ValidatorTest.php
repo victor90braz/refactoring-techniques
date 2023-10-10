77994dc9ddb92cf488af78447fadabd274198a4d
victor braz <victor.90braz@gmail.com>
2023-10-10 15:54:24 +0200

    validator test

--- tests/Unit/ValidatorTest.php
+++ tests/Unit/ValidatorTest.php
@@ -4,7 +4,19 @@ use Core\Validator;
 
 // $value, $min = 1, $max = INF
 it('validate a string', function() {
-  $result = Validator::string('foobar', 1, 10);
+  $result = Validator::string('foobar');
+
+  expect($result)->toBeTrue();
+});
+
+it('validates a string with minimum length', function() {
+  $result = Validator::string('foobar', 20);
+
+  expect($result)->toBeFalse();
+});
+
+it('validates a string with maximum length', function() {
+  $result = Validator::string('foobar', 1, 20);
 
   expect($result)->toBeTrue();
 });
